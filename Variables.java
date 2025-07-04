a139bde1296da098f6fff63e5ba41ebe3cbfc929
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-04 19:21:58 +0530

    variables in java

---
+++ Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+        // here we learn about variables
+
+
+        String name = "karni";    //name is variable here
+
+        int age = 21;            //age is variable here
+
+        String friend = "ankit";    //friend is variable here 
+
+        String dost = friend;    //dost is variable here //here i have same value for friend and dost so i called the friend variable in dost
+        
+        // now we print them all one by one 
+
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(friend);
+        System.out.println(dost); 
+    }
+}
