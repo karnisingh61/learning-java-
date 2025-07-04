ed3854898346abead857fab2253cc72995bac04c
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-04 19:39:52 +0530

    data type example

---
+++ DataTypeExample.java
@@ -0,0 +1,28 @@
+ // Java Program to demonstrate different Data Types
+
+public class DataTypeExample {
+    public static void main(String[] args) {
+
+        // Integer data type - stores whole numbers
+        int age = 21;
+
+        // Double data type - stores decimal numbers (floating point)
+        double price = 99.99;
+
+        // Char data type - stores a single character
+        char grade = 'A';
+
+        // Boolean data type - stores true or false values
+        boolean isJavaFun = true;
+
+        // String (non-primitive data type) - stores a sequence of characters
+        String name = "Hemant";
+
+        // Printing all variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java is fun: " + isJavaFun);
+    }
+}
