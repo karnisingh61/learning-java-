352286c9d924bfc4a7e3d4891eaf058b2bc5cd42
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-04 20:05:46 +0530

    Simple String Example

---
+++ StringSimpleExample.java
@@ -0,0 +1,11 @@
+public class StringSimpleExample {
+    public static void main(String[] args) {
+
+        //simple example of string
+        
+        String name = "Karni Singh";
+        String college = " BKBIET Pilani ";
+        System.out.println( "Student name :  "+name);
+        System.out.println("College name :   "+college);
+    }
+}
