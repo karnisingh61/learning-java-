c8201db977905e3c8e1a469ad226f9613bd262df
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-05 16:21:11 +0530

    example of airthematic operators in java

---
+++ AirthematicOperators.java
@@ -0,0 +1,18 @@
+public class AirthematicOperators {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        double c = 2.0;
+        double d = 5.0;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        double div = c / d;
+        int mod = a % b;
+        System.out.println("sum="+sum);
+         System.out.println("diffrence="+diff);
+          System.out.println("multiplication="+mul);
+           System.out.println("division="+div);
+            System.out.println("modulo="+mod);
+    }
+}
