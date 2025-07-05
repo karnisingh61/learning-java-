20e5b7559d1a58b0b8eb8fb665a097d6a5aa4fa9
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-05 16:28:15 +0530

    casting example

---
+++ CastingExamples.java
@@ -0,0 +1,15 @@
+public class CastingExamples {
+    public static void main(String[] args) {
+        
+
+        //implicit casting
+        double price = 200.00;
+        double finalprice = price + 18;
+        System.out.println(finalprice);
+
+        //explicit casting
+        int p = 100;
+        int fp = p +(int)18.99;
+        System.out.println(fp);
+    }
+}
