753f3200c025c20765171dbbe1533b532fff8df3
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-07 08:41:43 +0530

    example of Assignment Operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,26 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int x;
+
+        //=
+         x = 5;
+        System.out.println("x->" + x);
+
+        //+=
+         x += 3;
+        System.out.println("x->" + x);
+
+        //*= 
+        x *= 2;
+        System.out.println("x->" + x);
+
+        //-=
+        x -= 3;
+        System.out.println("x->" + x);
+
+        // /=
+        x /= 5;
+        System.out.println("x->" + x);
+
+    }
+}
