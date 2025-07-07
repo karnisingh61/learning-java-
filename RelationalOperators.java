02ccac964dcc304d1e574323fc882321d38bb988
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-07 08:57:49 +0530

    example of Relational Operators

---
+++ RelationalOperators.java
@@ -0,0 +1,26 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 5;
+         
+        //==
+        System.out.println(x==5);
+
+        //!=
+           System.out.println(x!= 3);
+           
+           //<=
+            System.out.println(x<=5);
+
+            //>=
+              System.out.println(x>=5);
+
+              //>
+                 System.out.println(x>4);
+
+                 //<
+                    System.out.println(x<7);
+
+
+    }
+    
+}
