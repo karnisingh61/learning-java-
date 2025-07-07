967ffe604fdb952b52370990c4bf8ca4509e0211
karnisingh61 <skarnisinghshekhawat@gmail.com>
2025-07-07 22:28:26 +0530

    example of loops in java

---
+++ Loops.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        
+        //for loop
+        for(int k=269; k<=300; k++){
+            System.out.println(k);
+        }
+
+        //while loop
+        int k = 269;
+        while(k<=280){
+            System.out.println(k);
+            k++;
+        }
+
+        //do while loop
+        int i=10;
+        do{
+System.out.println(i);
+i++;
+ 
+        }while(i<=50);
+    
+        
+    }
+    
+} 
